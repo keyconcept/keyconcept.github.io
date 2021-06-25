7cac7e884e229d75498dfb89953b65fcb99a1e49
SaKKo <saklism@gmail.com>
2021-06-25 10:48:50 +0700

    53caa76

---
+++ _nuxt/static/1624592927/projects/07/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624592927",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002F07",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ffdfee40"}}}}(null));
